a33f5e6158deba5dea39bdc8236c2f6c660338d4
Scott Taylor <scott@railsnewbie.com>
2010-06-07 01:23:15 -0400

    metacircular evaluator with unit tests

---
+++ sicp.scm
@@ -0,0 +1,119 @@
+#lang scheme/base
+
+; primitives to be defined in initial env:
+;   number?
+;   symbol?
+;   quote
+;   lambda
+;   car
+;   cdr
+;   cons
+
+(define eval
+  (lambda (expr env)
+    (cond
+     ((eq? expr #t) #t)
+     ((eq? expr #f) #f)
+     ((number? expr)
+      expr)
+     ((symbol? expr)
+      (lookup-symbol expr env))
+     ((eq? (car expr) 'quote)
+      (cadr expr))
+     ((eq? (car expr) 'cond)
+      (evcond (cdr expr) env))
+     ((eq? (car expr) 'lambda)
+      (list 'closure (cdr expr) env))
+     (else
+      (let ((fun (eval (car expr) env))
+            (args (cdr expr)))
+        (apply fun args))))))
+
+; internal functions are represented as
+;
+;   '(closure
+;     ((formal-params) body)
+;     env)
+(define apply
+  (lambda (internal-fun args)
+    (letrec ((fun (car (cdr internal-fun)))
+             (formal-params (car fun))
+             (body (car (cdr fun)))
+             (env (car (cdr (cdr internal-fun)))))
+      (eval body (bind formal-params args env)))))
+
+; symbols are stored as a stack, with each
+; key-value being a pair, with the most recent
+; scopes at the head of the list
+;
+;   i.e.:
+;
+; (let ((x 10))
+;   (let ((y 20))
+;     (let ((a x)
+;           (b y)))))
+;
+; scope:
+;  '((b y)
+;    (a x)
+;    (y 20)
+;    (x 10))
+;
+; empty scopes are not stored on the stack
+;
+; (bind '(x y) '(a b) '((a 10) (b 20)))
+(define empty?
+  (lambda (lst)
+    (eq? lst '())))
+
+(define bind
+  (lambda (vars values env)
+    (let ((empty-vars (empty? vars))
+          (empty-values (empty? values)))
+      (cond
+       ((and empty-vars empty-values) env)
+       (else
+        (bind (cdr vars)
+              (cdr values)
+              (cons-pair (car vars) (car values) env)))))))
+
+(define cons-pair
+  (lambda (key value lst)
+    (cons
+     (cons key
+           (cons value '()))
+     lst)))
+
+(define evcond
+  (lambda (lst env)
+    (cond
+     ((eq? lst '()) '())
+     (else
+      (letrec ((branch1 (car lst))
+               (branch2 (cdr lst))
+               (condition (car branch1))
+               (target (car (cdr branch1))))
+        (cond
+         ((eq? condition #t) target)
+         ((eq? condition 'else) target)
+         (else
+          (evcond branch2 env))))))))
+
+(define lookup-symbol
+  (lambda (expr env)
+    (cond
+     ((eq? env '())
+      (raise "unbound symbol"))
+     (else
+      (let ((first-pair (car env))
+            (other-pairs (cdr env)))
+        (let ((variable (car first-pair))
+              (value    (car (cdr first-pair))))
+          (cond
+           ((eq? variable expr)
+            (eval value env))
+           (else
+            (lookup-symbol expr other-pairs)))))))))
+
+
+(provide eval)
---
+++ sicp_test.scm
@@ -0,0 +1,138 @@
+#!/usr/bin/env mzscheme
+#lang scheme/base
+
+(require (planet schematics/schemeunit:3)
+         "sicp.scm")
+
+; test the framework
+(check-equal? #t #t "true is true")
+
+; true and false
+(check-equal? (eval '#t '()) #t "#t should eval to true")
+(check-equal? (eval '#f '()) #f "#f should eval to true")
+
+; test numbers
+(check-equal? (eval '1 '()) 1 "evaluating a number evaluates to itself")
+(check-equal? (eval '2 '()) 2 "evaluating 2 = the number 2")
+
+
+; test symbol usage
+(check-equal? (eval ''foo '())
+              'foo
+              "quoted symbols work")
+(check-equal? (eval ''bar '())
+              'bar
+              "a different quoted symbol works")
+(check-equal? (eval (quote (quote foo)) '())
+              'foo
+              "quoted symbols with quote work")
+
+; test cond
+(check-equal? (eval '(cond) '())
+              '()
+              "cond with no clauses should be the empty list")
+
+(check-equal? (eval '(cond (#t #t))
+                    '())
+              #t
+              "cond with one clause that is true should be true")
+
+(check-equal? (eval '(cond (#t #f))
+                    '())
+              #f
+              "cond with one clause that is true should return the correct conditional body")
+
+(check-equal? (eval '(cond (#f #f) (#t #t))
+                    '())
+              #t
+              "it should return the value of the first clause that matches")
+
+(check-equal? (eval '(cond (#f #f) (else #t))
+                    '())
+              #t
+              "it should match an else branch when nothing else succeeds")
+
+; lambda internal representation
+(check-equal? (eval '(lambda (x) x)
+                    '())
+              (list 'closure '((x) x) '())
+              "it should have the internal structure with 'closure, the formal params + body, and the env")
+
+
+; bound variables
+(check-equal? (eval 'foo
+                    '((foo #t)))
+              #t
+              "it should have a bound variable")
+
+(check-equal? (eval 'foo
+                    '((foo #f)))
+              #f
+              "it should use the correct value for the bound variable")
+
+(check-equal? (eval 'bar
+                    '((bar #f)))
+              #f
+              "it should use the correct bound variable for the bound variable")
+
+(check-equal? (eval 'foo
+                    '((bar #f)
+                      (foo #t)))
+              #t
+              "it shouldn't have to be the first on the stack")
+
+(check-equal? (eval 'foo
+                    '((bar #f)
+                      (foo (quote foo))))
+              'foo
+              "it should use the correct value for the variable when not on the top of the stack")
+
+
+; apply
+
+(check-equal? (eval '((lambda () #t))
+                    '())
+              #t
+              "it should return the value of calling the function")
+
+(check-equal? (eval '((lambda () #f))
+                   '())
+             #f
+             "it should return the correct value of the function")
+
+(check-equal? (eval '((lambda () (cond (#t #t))))
+                    '())
+              #t
+              "it should eval the body of the function")
+
+(check-equal? (eval '((lambda () x))
+                    '((x #t)))
+              #t
+              "it should eval the body of the function with variables from the outer scope")
+
+(check-equal? (eval '((lambda (x) x) #t)
+                    '())
+              #t
+              "it should eval the body of the function with variables from the inner scope (passed as params)")
+
+(check-equal? (eval '((lambda (y) y) #t)
+                    '())
+              #t
+              "it should eval the body of the function with the correct variable")
+
+(check-equal? (eval '((lambda (x) x) #f)
+                    '())
+              #f
+              "it should eval the body of the function with the correct values")
+
+(check-equal? (eval '((lambda (x y) y) 10 20)
+                    '())
+              20
+              "it should evaluate multiple arguments")
+
+; todo - implement macros:
+; (check-equal? (eval '((let ((x 10))
+;                         (lambda ()
+;                           x)))
+;               10)
+;               "it should use a variable in scope with let")
